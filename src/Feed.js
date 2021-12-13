bd33f81ea74a32483552fa2643b87c77826b9390
Ben Ndora <benndora2@gmail.com>
2021-12-13 15:11:46 +0300

    fixed bugs

--- src/Feed.js
+++ src/Feed.js
@@ -1,82 +1,79 @@
-import React, { useEffect } from 'react';
-import './Feed.css';
+import React, { useEffect } from "react";
+import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from "./InputOption";
-import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';import ImageIcon from "@material-ui/icons/Image";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import ImageIcon from "@material-ui/icons/Image";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
-import Post from "./Post"
-import {useState} from 'react';
-import {db} from "./Firebase";
-import firebase from 'firebase';
+import Post from "./Post";
+import { useState } from "react";
+import { db } from "./Firebase";
+import firebase from "firebase/compat/app";
 
 function Feed() {
- 
-    const [input,setInput] = useState([]);
-    const [posts,setPosts] = useState([]);  //eslint-disable-line
-  
-    useEffect(()=> {
-        db.collection('posts').onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => (
-                  {
-                      id: doc.id,
-                      data: doc.data()
-                  }
-            )))
-        ))
-    }, [])
+  const [input, setInput] = useState([]);
+  const [posts, setPosts] = useState([]); //eslint-disable-line
 
-    const sendPost =  e => {
-        e.preventDefault();
-       db.collection('posts').add({
-           name: 'Benson Ndora',
-           description: 'This is a test',
-           message: input,
-           photoUrl: '',
-           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-       })
-    }
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
 
-    return (
-        <div className="feed">
-            <div className="feed__inputContainer">
-                <div className="feed__input">
-                    <CreateIcon/>
-                        <form>
-                        <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
-                        <button onClick={sendPost} type="submit">Send</button>
-                         
-                      </form>
-                </div>
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: "Benson Ndora",
+      description: "This is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
 
-                 <div className="feed__inputOptions">
-                     <InputOption Icon={ImageIcon} 
-                     title="Photo" color="#70B5F9"/>
-                    <InputOption Icon={SubscriptionsIcon}
-                     title="Video"
-                    color="#E7A33E"/>
-                      <InputOption Icon={EventNoteIcon} 
-                      title="Event"
-                      color="#C0C8CD"/>
-                    <InputOption Icon={CalendarViewDayIcon} 
-                    title="Write Article"
-                    color="#7FC15E"/>
-                 </div>
-
-            </div>
-
-            {/* posts */}
-            {posts.map((post) => (
-                <Post/> 
-             ))}
-           
-            <Post name="Benson"
-            description="This is test"
-            message="iT WORKED "
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            
-        </div> 
-    )
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0C8CD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+
+      {/* posts */}
+      {posts.map((post) => (
+        <Post />
+      ))}
+
+      <Post name="Benson" description="This is test" message="iT WORKED " />
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,51 +1,46 @@
-import { Avatar} from "@material-ui/core";
-import React from 'react';
-import './Sidebar.css';
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar_harsh">#</span>
+      <p>{topic}</p>
+    </div>
+  );
 
-   const recentItem= (topic) => (
-       <div className="sidebar__recentItem">
-           <span className="sidebar_harsh">#</span>
-           <p>{topic}</p>
-
-       </div>
-   )
-
-
-
-
-
-
-    return (
-        <div className="sidebar">
-            <div className="sidebar__top">
-                <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fD88MHxleHBsb3JlLWZlZwR8MXx8fGVufD88fHw%3D&w=1000&q=80" alt=""/>
-                <Avatar/>
-                <h2>Benson Ndora</h2>
-                <h4>Benndora2@gmail.com</h4>
-                </div>
-                <div className="sidebar__stats">
-                    <div className="sidebar__stat">
-                             <p>Who viewed You</p>
-                             <p className="sidebar__statNumber">2,543</p>
-                    </div>
-                    <div className="sidebar__stat">
-                                <p>Viewed Posts</p>
-                                <p className="sidebar__statNumber">2,456</p>
-                    </div>
-                </div>
-
-                <div className="sidebar__bottom">
-                    <p>Recent</p>
-                    {recentItem("Reactjs")}
-                    {recentItem("Programming")}
-                    {recentItem("Web Design")}
-                    {recentItem("Web Production")}
-                </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fD88MHxleHBsb3JlLWZlZwR8MXx8fGVufD88fHw%3D&w=1000&q=80"
+          alt=""
+        />
+        <Avatar />
+        <h2>Benson Ndora</h2>
+        <h4>Benndora2@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Your Views</p>
+          <p className="sidebar__statNumber">2,543</p>
         </div>
-    )
+        <div className="sidebar__stat">
+          <p>Viewed Posts</p>
+          <p className="sidebar__statNumber">2,456</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("Reactjs")}
+        {recentItem("Programming")}
+        {recentItem("Web Design")}
+        {recentItem("Web Production")}
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
-   
+export default Sidebar;
