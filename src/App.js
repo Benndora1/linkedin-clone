f6a2099137eaefd06c9bb280329230fc0a9f74f3
benndora1 <benndora2@gmail.com>
2022-01-06 17:18:35 +0300

    keep the session

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Login from "./Login";
-import { selectUser } from "./features/userSlice";
+import { selectUser, login, logout} from "./features/userSlice";
+import { auth } from "./Firebase";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  //persisting the logged in user
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+
+        // user is logged in
+      } else {
+
+        // user is logged out
+        dispatch(logout());
+      }
+
+    })
+  })
 
   return (
     <div className="app">
